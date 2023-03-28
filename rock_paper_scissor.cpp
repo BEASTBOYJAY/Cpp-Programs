d76759c5e72f1a9a503af465b5cc1b9e6d86eecb
Jay Sinha <128999646+BEASTBOYJAY@users.noreply.github.com>
2023-03-28 20:07:52 +0530

    Update rock_paper_scissor.cpp

--- rock_paper_scissor.cpp
+++ rock_paper_scissor.cpp
@@ -7,76 +7,82 @@ int main()
     srand(time(NULL));
     int user=0;
     int cpu=0;
+    int round;
     cout<<"        ROCK PAPER SCISSORS SHOOT         "<<endl;
-    cout<<"1) FOR ROCK "<<endl;
-    cout<<"2) FOR PAPER "<<endl;
-    cout<<"3) FOR SCISSORS "<<endl;
-    cin>>user;
-    if(user==1)
-    {
-        cout<<"YOU CHOOSE ROCK"<<endl;
-    }
-    else if(user==2)
-    {
-        cout<<"YOU CHOOSE PAPER"<<endl;
-    }
-    else
-    {
-        cout<<"YOU CHOOSE SCISSORS"<<endl;
-    }
-    cpu=rand()%3+1;
-    if(cpu==1)
-    {
-        cout<<"CPU CHOOSE ROCK"<<endl;
-    }
-    else if(cpu==2)
-    {
-        cout<<"CPU CHOOSE PAPER"<<endl;
-    }
-    else
-    {
-        cout<<"CPU CHOOSE SCISSORS"<<endl;
-    }
-    // for tie
-    if(user==cpu)
-    {
-        cout<<"-------------GAME TIE---------------"<<endl;
-    }
-    // for rock
-    if (user==1)
-    {
-        if(cpu==2)
+    cout<<"enter no. of rounds"<<endl;
+    cin>>round;
+    int i=0;
+    while(i<round){
+        cout<<"1) FOR ROCK "<<endl;
+        cout<<"2) FOR PAPER "<<endl;
+        cout<<"3) FOR SCISSORS "<<endl;
+        cin>>user;
+        if(user==1)
         {
-            cout<<"YOU LOSE!!!"<<endl;
+            cout<<"YOU CHOOSE ROCK"<<endl;
         }
-        if (cpu==3)
+        else if(user==2)
         {
-            cout<<"YOU WIN!!!"<<endl;
+            cout<<"YOU CHOOSE PAPER"<<endl;
         }
-    }
-    // for paper 
-    else if (user==2)
-    {
+        else
+        {
+            cout<<"YOU CHOOSE SCISSORS"<<endl;
+        }
+        cpu=rand()%3+1;
         if(cpu==1)
         {
-            cout<<"YOU WIN!!!"<<endl;
+            cout<<"CPU CHOOSE ROCK"<<endl;
         }
-        if(cpu==3)
+        else if(cpu==2)
         {
-            cout<<"YOU LOSE!!!"<<endl;
+            cout<<"CPU CHOOSE PAPER"<<endl;
         }
-    }
-    // for scissor
-    else if (user==3)
-    {
-        if(cpu==1)
+        else
         {
-            cout<<"YOU LOSE!!!"<<endl;
+            cout<<"CPU CHOOSE SCISSORS"<<endl;
         }
-        if(cpu==2)
+        // for tie
+        if(user==cpu)
         {
-            cout<<"YOU WIN!!!"<<endl;
+            cout<<"-------------GAME TIE---------------"<<endl;
         }
+        // for rock
+        if (user==1)
+        {
+            if(cpu==2)
+            {
+                cout<<"YOU LOSE!!!"<<endl;
+            }
+            if (cpu==3)
+            {
+                cout<<"YOU WIN!!!"<<endl;
+            }
+        }
+        // for paper 
+        else if (user==2)
+        {
+            if(cpu==1)
+            {
+                cout<<"YOU WIN!!!"<<endl;
+            }
+            if(cpu==3)
+            {
+                cout<<"YOU LOSE!!!"<<endl;
+            }
+        }
+        // for scissor
+        else if (user==3)
+        {
+            if(cpu==1)
+            {
+                cout<<"YOU LOSE!!!"<<endl;
+            }
+            if(cpu==2)
+            {
+                cout<<"YOU WIN!!!"<<endl;
+            }
+        }i++;
     }
     return 0;
 }
